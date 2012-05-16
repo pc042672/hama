b00f4b2361b5d6e71831010bdfd18e63cdf4003a
ssmenon <menonsuraj5@gmail.com>
2012-05-16 04:25:05 -0300

    Update hama-mapreduce/src/org/apache/hama/computemodel/mapreduce/ShuffleAndDistribute.java

--- hama-mapreduce/src/org/apache/hama/computemodel/mapreduce/ShuffleAndDistribute.java
+++ hama-mapreduce/src/org/apache/hama/computemodel/mapreduce/ShuffleAndDistribute.java
@@ -9,6 +9,7 @@ import java.util.PriorityQueue;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapred.Partitioner;
@@ -21,104 +22,91 @@ import org.apache.hama.bsp.sync.SyncException;
 import org.apache.hama.computemodel.mapreduce.ReducerKeyDesignator.DesignateStrategy;
 import org.apache.hama.computemodel.mapreduce.ReducerKeyDesignator.KeyDistribution;
 
-public class 
-ShuffleAndDistribute<K1 extends WritableComparable<K1>,V1,K2 extends WritableComparable<K2>,V2 extends Writable, M> extends
-Superstep<K1, V1, K2, V2, WritableKeyValues<? extends Writable, ? extends Writable>>{
+public class ShuffleAndDistribute<K2 extends WritableComparable<?>, V2 extends Writable>
+    extends
+    Superstep<NullWritable, NullWritable, K2, V2, WritableKeyValues<? extends WritableComparable<?>, ? extends Writable>> {
 
-  //private Map<Long, Long> keyDistributionMap = new HashMap<Long, Long>();
+  // private Map<Long, Long> keyDistributionMap = new HashMap<Long, Long>();
   private long[][] globalKeyDistribution;
   private PriorityQueue<WritableKeyValues<K2, V2>> memoryQueue;
 
   @Override
   protected void setup(
-      BSPPeer<K1, V1, K2, V2, WritableKeyValues<? extends Writable, ? extends Writable>> peer) {
-
+      BSPPeer<NullWritable, NullWritable, K2, V2, WritableKeyValues<? extends WritableComparable<?>, ? extends Writable>> peer) {
+    // TODO Auto-generated method stub
     super.setup(peer);
-    this.memoryQueue = (PriorityQueue<WritableKeyValues<K2, V2>>)
-        peer.getSavedObject(Mapper.MESSAGE_QUEUE);
-    
-    this.globalKeyDistribution = 
-        (long[][])peer.getSavedObject(Mapper.KEY_DIST);
+    this.memoryQueue = (PriorityQueue<WritableKeyValues<K2, V2>>) peer
+        .getSavedObject(Mapper.MESSAGE_QUEUE);
+
+    this.globalKeyDistribution = (long[][]) peer
+        .getSavedObject(Mapper.KEY_DIST);
   }
-  
-  
-  
-  protected void designateKeysToReducers(int[] keyDistribution, 
-      final long[][] globalKeyDistribution, Configuration conf){
+
+  protected void designateKeysToReducers(int[] keyDistribution,
+      final long[][] globalKeyDistribution, Configuration conf) {
     Class<?> designatorClass = conf.getClass("", null);
     ReducerKeyDesignator designator = null;
-    if(designatorClass == null){
+    if (designatorClass == null) {
       designator = ReducerKeyDesignator.getReduceDesignator(
-          DesignateStrategy.MINIMIZE_COMMUNICATION, 
-          KeyDistribution.CONTIGUOUS);
+          DesignateStrategy.MINIMIZE_COMMUNICATION, KeyDistribution.CONTIGUOUS);
+    } else {
+      designator = (ReducerKeyDesignator) (ReflectionUtils.newInstance(
+          designatorClass, conf));
     }
-    else{
-      designator = (ReducerKeyDesignator) 
-          (ReflectionUtils.newInstance(designatorClass, conf));
-    }
-    
-    designator.designateKeysToReducers(
-        keyDistribution, globalKeyDistribution, conf);
-    
+
+    designator.designateKeysToReducers(keyDistribution, globalKeyDistribution,
+        conf);
+
   }
 
   @Override
   protected void compute(
-      BSPPeer<K1, V1, K2, V2, WritableKeyValues<? extends Writable, ? extends Writable>> peer)
-          throws IOException {
+      BSPPeer<NullWritable, NullWritable, K2, V2, WritableKeyValues<? extends WritableComparable<?>, ? extends Writable>> peer)
+      throws IOException {
     int peerId = peer.getPeerId();
     Configuration conf = peer.getConfiguration();
-    
+
     WritableKeyValues<WritableKeyValues<IntWritable, IntWritable>, LongWritable> message;
-    while((message = (WritableKeyValues<WritableKeyValues<IntWritable, IntWritable>, LongWritable>) 
-        peer.getCurrentMessage()) != null){
+    while ((message = (WritableKeyValues<WritableKeyValues<IntWritable, IntWritable>, LongWritable>) peer
+        .getCurrentMessage()) != null) {
       int peerNo = message.getKey().getKey().get();
       int partition = message.getKey().getValue().get();
       globalKeyDistribution[peerNo][partition] += message.getValue().get();
     }
 
     int[] keyDistribution = new int[globalKeyDistribution[0].length];
-    
+
     designateKeysToReducers(keyDistribution, globalKeyDistribution, conf);
-    
+
     int myKeyCount = 0;
-    for(int i = 0; i < globalKeyDistribution[0].length; ++i){
+    for (int i = 0; i < globalKeyDistribution[0].length; ++i) {
       myKeyCount += globalKeyDistribution[peerId][i];
     }
-    
-    PriorityQueue<WritableKeyValues<K2, V2>> mergeQueue = 
-        new PriorityQueue<WritableKeyValues<K2, V2>>(myKeyCount);
-    Partitioner<K2, V2> partitioner = (Partitioner<K2, V2>) ReflectionUtils.newInstance(
-        conf.getClass(Mapper.PARTITIONER_CLASS, HashPartitioner.class), conf);
-    
-    Iterator<WritableKeyValues<K2, V2>> keyValIter = this.memoryQueue.iterator();
+
+    PriorityQueue<WritableKeyValues<K2, V2>> mergeQueue = new PriorityQueue<WritableKeyValues<K2, V2>>(
+        myKeyCount);
+    Partitioner<K2, V2> partitioner = (Partitioner<K2, V2>) ReflectionUtils
+        .newInstance(
+            conf.getClass(Mapper.PARTITIONER_CLASS, HashPartitioner.class),
+            conf);
+
+    Iterator<WritableKeyValues<K2, V2>> keyValIter = this.memoryQueue
+        .iterator();
     String[] peerNames = peer.getAllPeerNames();
-    while(keyValIter.hasNext()){
+    while (keyValIter.hasNext()) {
       WritableKeyValues<K2, V2> record = keyValIter.next();
-      int partition = partitioner.getPartition(record.getKey(), 
-          record.getValue(), 
-          peer.getNumPeers()); // should be num reducers eventually
+      int partition = partitioner.getPartition(record.getKey(),
+          record.getValue(), peer.getNumPeers()); // should be num reducers
+                                                  // eventually
       int destPeerId = keyDistribution[partition];
-      if(peerId != destPeerId){
+      if (peerId != destPeerId) {
         peer.send(peerNames[destPeerId], record);
         keyValIter.remove();
       }
     }
-        
-    
-    try {
-      peer.sync();
-    } catch (SyncException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+
+
   }
 
-
-
-
 }
-//K1 extends WritableComparable<K1>,V1,K2 extends WritableComparable<K2>,V2 extends Writable, M extends WritableKeyValues<K2, V2>
+
