e0973ea0f8a5b3425f11fcae4157aa0b105256f5
ssmenon <menonsuraj5@gmail.com>
2012-05-16 04:25:59 -0300

    Update hama-mapreduce/src/org/apache/hama/computemodel/mapreduce/Reducer.java

--- hama-mapreduce/src/org/apache/hama/computemodel/mapreduce/Reducer.java
+++ hama-mapreduce/src/org/apache/hama/computemodel/mapreduce/Reducer.java
@@ -6,97 +6,81 @@ import java.util.Iterator;
 import java.util.PriorityQueue;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.apache.hama.bsp.BSPPeer;
 import org.apache.hama.bsp.Superstep;
 import org.apache.hama.computemodel.mapreduce.Mapper.CombinerOutputCollector;
 
-public class Reducer<K2 extends Writable, V2 extends Writable, K3 extends Writable, V3 extends Writable> 
-extends Superstep<K2, V2, K3, V3, WritableKeyValues<K2, V2>> {
-	
-	public static final String REDUCER_CLASS = "hama.mapreduce.reducer";
+public abstract class Reducer<K2 extends WritableComparable<?>, V2 extends Writable, K3 extends WritableComparable<?>, V3 extends Writable>
+    extends Superstep<K2, V2, K3, V3, WritableKeyValues<K2, V2>> {
 
-	private PriorityQueue<WritableKeyValues<K2, V2>> memoryQueue;
+  public static final String REDUCER_CLASS = "hama.mapreduce.reducer";
 
-	@Override
-	protected void compute(
-			BSPPeer<K2, V2, K3, V3, WritableKeyValues<K2, V2>> peer)
-			throws IOException {
-		
-		this.memoryQueue = (PriorityQueue<WritableKeyValues<K2, V2>>) 
-				peer.getSavedObject(Mapper.MESSAGE_QUEUE);
-		
-		Configuration conf = peer.getConfiguration();
-		
-		WritableKeyValues<K2, V2> message;
-		while((message = peer.getCurrentMessage()) != null){
-			this.memoryQueue.add(message);
-		}
-		
-		CombinerOutputCollector<K2, V2> outputCollector = 
-				new CombinerOutputCollector<K2, V2>();
-		Comparator<V2> valComparator = null;
-	    Class<?> comparatorClass = 
-	    		conf.getClass(Mapper.VALUE_COMPARATOR_CLASS, null);
-	    if (comparatorClass != null) {
-	      valComparator = (Comparator<V2>) ReflectionUtils.newInstance(
-	          comparatorClass, conf);
-	    }
+  private PriorityQueue<WritableKeyValues<K2, V2>> memoryQueue;
 
-	    	    Iterator<WritableKeyValues<K2, V2>> recordIterator = memoryQueue.iterator();
+  @Override
+  protected void compute(BSPPeer<K2, V2, K3, V3, WritableKeyValues<K2, V2>> peer)
+      throws IOException {
 
-	      CombinerOutputCollector<K2, V2> collector 
-	      					= new CombinerOutputCollector<K2, V2>();
+    this.memoryQueue = (PriorityQueue<WritableKeyValues<K2, V2>>) peer
+        .getSavedObject(Mapper.MESSAGE_QUEUE);
 
-	      WritableKeyValues<K2, V2> previousRecord = null;
+    Configuration conf = peer.getConfiguration();
 
-	      while (recordIterator.hasNext()) {
+    WritableKeyValues<K2, V2> message;
+    while ((message = peer.getCurrentMessage()) != null) {
+      this.memoryQueue.add(message);
+    }
 
-	        WritableKeyValues<K2, V2> record = recordIterator.next();
-	        K2 key = record.getKey();
-	        if (previousRecord != null 
-	            && key.equals(previousRecord.getKey())) {
-	          previousRecord.addValues(record.getValues());
-	          recordIterator.remove();
-	        } else {
-	          if (previousRecord != null) {
-	            previousRecord.sortValues(valComparator);
-	          }
-	          previousRecord = record;
-	        }
-	      }
+    CombinerOutputCollector<K3, V3> outputCollector = new CombinerOutputCollector<K3, V3>();
+    Comparator<V2> valComparator = null;
+    Class<?> comparatorClass = conf.getClass(Mapper.VALUE_COMPARATOR_CLASS,
+        null);
+    if (comparatorClass != null) {
+      valComparator = (Comparator<V2>) ReflectionUtils.newInstance(
+          comparatorClass, conf);
+    }
 
-	      org.apache.hadoop.mapred.Reducer<K2, V2, K2, V2> reducer = null;
-		  Class<?> reducerClass = conf.getClass(REDUCER_CLASS, null);
+    Iterator<WritableKeyValues<K2, V2>> recordIterator = memoryQueue.iterator();
 
-		    if (reducerClass != null) {
-		    	reducer = (org.apache.hadoop.mapred.Reducer<K2, V2, K2, V2>) 
-		    		  ReflectionUtils.newInstance(reducerClass, conf);
-		 }
-		    
-		 if(reducer != null){
-			 Iterator<WritableKeyValues<K2, V2>> recordIter = 
-					 this.memoryQueue.iterator();
-			 while(recordIter.hasNext()){
-				 WritableKeyValues<K2,V2> record = recordIter.next();
-				 Iterator<V2> valIterator = record.getValues().iterator();
-				 reducer.reduce(record.getKey(), valIterator, output, null);
-				 
-			 }
-		 }
-			
+    WritableKeyValues<K2, V2> previousRecord = null;
 
-		
-		
-	}
-	
-	
-	
-	
-	
+    while (recordIterator.hasNext()) {
 
+      WritableKeyValues<K2, V2> record = recordIterator.next();
+      K2 key = record.getKey();
+      if (previousRecord != null && key.equals(previousRecord.getKey())) {
+        previousRecord.addValues(record.getValues());
+        recordIterator.remove();
+      } else {
+        if (previousRecord != null) {
+          previousRecord.sortValues(valComparator);
+        }
+        previousRecord = record;
+      }
+    }
+
+    // if(reducer != null){
+    Iterator<WritableKeyValues<K2, V2>> recordIter = this.memoryQueue
+        .iterator();
+    while (recordIter.hasNext()) {
+      WritableKeyValues<K2, V2> record = recordIter.next();
+      Iterator<V2> valIterator = record.getValues().iterator();
+      reduce(record.getKey(), valIterator, outputCollector);
+    }
+
+    Iterator<WritableKeyValues<K3, V3>> outputIter = outputCollector
+        .getCollectedRecords().iterator();
+    while (outputIter.hasNext()) {
+      WritableKeyValues<K3, V3> output = outputIter.next();
+      peer.write(output.getKey(), output.getValue());
+    }
+    // }
+  }
+
+  public abstract void reduce(K2 key, Iterator<V2> values,
+      OutputCollector<K3, V3> output) throws IOException;
 }
